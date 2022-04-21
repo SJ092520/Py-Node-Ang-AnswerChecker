2dac5bbe4f6b89824f79d10ed6087ff982bed8bf
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-21 23:56:53 +0530

    Added api services for user

--- Frontend/src/app/service/api-client.service.ts
+++ Frontend/src/app/service/api-client.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from "./../../environments/environment";
+import { getHeader, getHeaderForUpload, getHeaderForDownload } from '../utils/auth.utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiClientService {
+  host: string = environment.apiURL;
+  header: any;
+  constructor(private http: HttpClient) {
 
-  constructor() { }
+  }
+  throwError(err: any): string {
+    let message: string = err.error.errorMessage;
+    throw message || "Please f-try later";
+  }
+  //USER
+  getAllUser(): Observable<any> {
+    return this.http.get<any>(`${this.host}/user/profile/all`, getHeader())
+      .pipe(catchError(err => this.throwError(err)));
+  }
+
+  loginUser(loginDetails: any): Observable<any> {
+    return this.http.post<any>(`${this.host}/user/login`, loginDetails)
+      .pipe(catchError(err => this.throwError(err)));
+  }
+
+  createUser(userDetails: any): Observable<any> {
+    return this.http.post<any>(`${this.host}/user/create`, userDetails, getHeader())
+      .pipe(catchError(err => this.throwError(err)));
+  }
+
+  editUser(userDetails: any, userId: string): Observable<any> {
+    return this.http.put<any>(`${this.host}/user/edit/${userId}`, userDetails, getHeader())
+      .pipe(catchError(err => this.throwError(err)));
+  }
+  //STUDENT
+
+  getUserById(userId: string): Observable<any> {
+    return this.http.get<any>(`${this.host}/student/profile/${userId}`, getHeader())
+      .pipe(catchError(err => this.throwError(err)));
+  }
 }
