6771c56f97de62f8d8b88cc1ac437863433bc653
vaivassudhan <vaivassaravanan1@gmail.com>
2022-04-21 23:13:18 +0530

    student id

--- Backend/model/student.model.js
+++ Backend/model/student.model.js
@@ -4,4 +4,10 @@ const { COLLECTION_NAME } = require('../keys/constants');
 const studentModel = {}
 
 
+studentModel.getStudentById = studentId => {
+    return collection.getCollection(COLLECTION_NAME.STUDENT)
+        .then(model => model.findOne({studentId}))
+        .then(response =>  response);
+}
+
 module.exports = studentModel;
--- Backend/service/student.service.js
+++ Backend/service/student.service.js
@@ -7,4 +7,14 @@ const { JWT_KEY } = require("../keys/constants");
 
 const studentService ={};
 
+studentService.getStudentById() = (studentId) => {
+    return studentModel.getStudentById(studentId)
+        .then(response => {
+            if(response) {
+                return response;
+            }
+            throw new ApiError("Student not found", 404);
+        });
+    }
+
 module.exports = studentService;
