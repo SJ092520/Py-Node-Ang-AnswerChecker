8ea3328fcfe2d757d73e28797dc636af54bb6640
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-21 22:29:23 +0530

    globalStore

--- Frontend/src/app/service/global-store.service.ts
+++ Frontend/src/app/service/global-store.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class GlobalStoreService {
 
   constructor() { }
+  getGlobalStore() {
+    return {
+      userId: localStorage.getItem('USERID'),
+      userName: localStorage.getItem('USERNAME'),
+      userType: localStorage.getItem('USERTYPE')
+    }
+  }
+  setGlobalStore(globalData: any) {
+    localStorage.setItem('USERID', globalData.userId);
+    localStorage.setItem('USERNAME', globalData.userName);
+    localStorage.setItem('USERTYPE', globalData.userType);
+  }
+  clearGlobalStore() {
+    localStorage.clear();
+  }
 }
