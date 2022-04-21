9e28990f27c71d6cc109956f1d7816eea9b5f759
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-21 22:45:09 +0530

    AUTH GUARD SERVICE

--- Frontend/src/app/service/auth-guard.service.ts
+++ Frontend/src/app/service/auth-guard.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
+import { Router, CanActivateChild, CanActivate } from '@angular/router';
+
+import { GlobalStoreService } from './global-store.service';
+
+import { hasAdminAccess } from '../utils/auth.utils';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate, CanActivateChild {
 
-  constructor() { }
+  constructor(private route: Router) { }
+  canActivate(): Boolean {
+    if (localStorage.getItem("TOKEN") === null) return true;
+    this.route.navigate(['/home']);
+    return false;
+  }
+  canActivateChild(): Boolean {
+    if (localStorage.getItem("TOKEN") !== null) return true;
+    this.route.navigate(['/auth/login']);
+    return false;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuardService implements CanActivate {
+  constructor(private route: Router, private globalStore: GlobalStoreService) { }
+  canActivate(): boolean {
+    const { userType } = this.globalStore.getGlobalStore();
+    if (hasAdminAccess(userType)) return true;
+    this.route.navigate(['/home']);
+    return false;
+  }
 }
