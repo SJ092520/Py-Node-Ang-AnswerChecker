ce6097c2abc6bb564554451a53ae64d41e635b22
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-21 22:48:16 +0530

    ADDED SERVICES

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ApiClientService } from './service/api-client.service';
+import { AuthGuardService } from './service/auth-guard.service';
+import { GlobalStoreService } from './service/global-store.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ApiClientService,
+    AuthGuardService,
+    GlobalStoreService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
