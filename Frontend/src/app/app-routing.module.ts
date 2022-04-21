0a980b9a807c13102002e1992118fa495507a839
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-22 00:00:22 +0530

    Routing module fixes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ContentLayoutComponent} from "./layout/content-layout/content-layout.component";
+import { ContentLayoutComponent } from "./layout/content-layout/content-layout.component";
 
 import { AuthGuardService, AdminGuardService } from './service/auth-guard.service';
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/auth/login',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: ContentLayoutComponent,
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: '',
-    component: ContentLayoutComponent, 
-    canActivateChild:[AuthGuardService],
+    component: ContentLayoutComponent,
+    canActivateChild: [AuthGuardService],
     children: [
 
-      
+
     ]
   },
   { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
