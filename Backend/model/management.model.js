37dbfe59b8f896fca7238768159740dd1a19e68b
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-22 12:40:31 +0530

    Management model get user by id

--- Backend/model/management.model.js
+++ Backend/model/management.model.js
@@ -3,5 +3,9 @@ const { COLLECTION_NAME } = require('../keys/constants');
 
 const managementModel = {}
 
-
+managementModel.getManagementById = id => {
+    return collection.getCollection(COLLECTION_NAME.MANAGEMENT)
+        .then(model => model.findOne({ id }))
+        .then(response => response);
+}
 module.exports = managementModel;
