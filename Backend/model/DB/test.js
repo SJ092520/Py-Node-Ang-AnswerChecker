9bc8b02daab4db665407aaaa0e1904b0a278c2c6
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-23 21:08:13 +0530

    student schema redesign

--- Backend/model/DB/test.js
+++ Backend/model/DB/test.js
@@ -5,7 +5,7 @@ const userModel = require("../user.model");
 const managementModel = require("../management.model");
 const studentModel = require("../student.model");
 
-userModel.createUser({ "userId": "1518102126", "password": "1234", "role": 2 });
-managementModel.create({ id: "1518MOH", name: "Mohanraj", deptId: "CSE", designation: "Assistant Professor", email: "mohan@", phone: "1234567890" });
-studentModel.create({ id: "1518102126", name: "senthil", deptId: "CSE", email: "mohan@", phone: "1234567890" });
+// userModel.createUser({ "userId": "1518102126", "password": "1234", "role": 2 });
+// managementModel.create({ id: "1518MOH", name: "Mohanraj", deptId: "CSE", designation: "Assistant Professor", email: "mohan@", phone: "1234567890" });
+studentModel.create({ id: "1518102126", name: "senthil", deptId: "CSE", email: "mohan@", phone: "1234567890", batch: 2022 });
 console.log("created")
--- Backend/model/schemas/student.js
+++ Backend/model/schemas/student.js
@@ -5,22 +5,35 @@ const studentSchema = {
     "phone": { type: String, required: true },
     "deptId": { type: String, required: true },
     "role": { type: String, required: true, default: 2 },
-    "semester": {
-        type: {
-            "semesterId": {
-                type: [
-                    {
-                        type: {
-                            "semYear": { type: String, required: true },
-                            "courseId": { type: String, required: true },
-                            "marks": { type: { "internal": { type: Number }, "external": { type: Number } } },
-                            "isElective": { type: Boolean, required: true },
-                        }
-                    }
-                ]
+    "batch": { type: Number, required: true, Range: [1990, 2100] },
+    "semester": [{
+        "course": [{
+            "year": { type: String, required: true },
+            "courseId": { type: String, required: true },
+            "courseYearSemId": { type: String, required: true },
+            "marks": {
+                type: {
+                    "internal": { type: Number },
+                    "external": { type: Number }
+                }
             },
-        }
-    },
+            "answerSheet": {
+                type: {
+                    "question": [{
+                        "questionId": { type: String, required: true },
+                        "answer": { type: String, required: true },
+                        "mark": { type: Number, required: true },
+                    }],
+                    "total": { type: Number, required: true },
+                    "fileName": { type: String, required: true },
+                    "filePath": { type: String, required: true }
+                }
+            },
+            "isElective": { type: Boolean, required: true },
+            "arrear": { type: Boolean, required: true, default: false },
+            "uploadDate": { type: Date, required: true, default: Date.now() },
+        }]
+    }],
 };
 module.exports = studentSchema;
 
