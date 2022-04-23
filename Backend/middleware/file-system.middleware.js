8ed869406d23b1224c057f28477a4429da61d411
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-23 21:09:28 +0530

    Changes in file system middleware

--- Backend/middleware/file-system.middleware.js
+++ Backend/middleware/file-system.middleware.js
@@ -2,61 +2,53 @@ const multer = require('multer');
 const fs = require('fs');
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) =>{
+    destination: (req, file, callback) => {
         const path = `./FileSystem/${req.folderPath}`;
         let folderNames = path.split('/');
         let currentPath = '.';
         let i = 1;
-        do{     
+        do {
             currentPath += `/${folderNames[i]}`;
-            if (!fs.existsSync(currentPath)){
+            if (!fs.existsSync(currentPath)) {
                 fs.mkdirSync(currentPath);
             }
             ++i;
-        }while(i < folderNames.length);
+        } while (i < folderNames.length);
         callback(null, path)
     },
-    filename:(req, file, callback) =>{
+    filename: (req, file, callback) => {
         let fullName = file.originalname;
-        let fileName = fullName.split(".")[0] 
+        let fileName = fullName.split(".")[0]
         let ext = fullName.split(".")[1];
-        callback(null,`${fileName}-${Date.now()}.${ext}`)
+        callback(null, `${fileName}-${Date.now()}.${ext}`)
     }
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 
-const setPathFilledApplication = (req,res,next) =>{ 
-    try{
-        req.folderPath = `/FundingProject/${req.params.fundingProjectId}/FilledApplication`;
+const setPathAnswerSheet = (req, res, next) => {
+    try {
+        req.folderPath = `/${req.params.courseYearSem}/AnswerSheet/${req.params.studentId}`;
         next();
-    }catch(error){
+    } catch (error) {
         throw new ApiError(error, 403);
     }
 };
-const setPathprojectApplication = (req,res,next) =>{ 
-    try{
-        req.folderPath = `/Project/${req.params.projectId}/File`;
+const setPathAnswerKey = (req, res, next) => {
+    try {
+        req.folderPath = `/${req.params.courseYearSem}/AnswerKey`;
         next();
-    }catch(error){
+    } catch (error) {
         throw new ApiError(error, 403);
     }
 };
-const setPathPublicationApplication = (req,res,next) =>{ 
-    try{
-        req.folderPath = `/Publication/${req.params.publicationId}/File`;
+const setPathQuestionPaper = (req, res, next) => {
+    try {
+        req.folderPath = `/${req.params.courseYearSem}/QuestionPaper`;
         next();
-    }catch(error){
-        throw new ApiError(error, 403);
-    }
-};
-const setPathAck = (req,res,next) =>{ 
-    try{
-        req.folderPath = `/FundingProject/${req.params.fundingProjectId}/Acknowledgement`;
-        next();
-    }catch(error){
+    } catch (error) {
         throw new ApiError(error, 403);
     }
 };
 
-module.exports = {upload, setPathprojectApplication,setPathFilledApplication, setPathAck,setPathPublicationApplication};
+module.exports = { upload, setPathAnswerKey, setPathAnswerSheet, setPathQuestionPaper };
