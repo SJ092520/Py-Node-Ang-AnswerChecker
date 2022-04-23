6064eb49b83efe818a31cb024672da47a3cc9ebb
Shreinik Jain <1029384756hhh@gmail.com>
2022-04-23 21:08:35 +0530

    Added file upload routes in student and management

--- Backend/routes/management.routes.js
+++ Backend/routes/management.routes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const userService = require("../service/management.service");
 const { studentAuth, adminAuth } = require('../middleware/auth.middleware');
+const { setPathAnswerKey, upload } = require("../middleware/file-system.middleware");
 
 const managementRouter = express.Router();
 
+managementRouter.put("/answer-key/:courseYearSem",
+    studentAuth,
+    setPathAnswerKey,
+    upload.single('file'),
+    (req, res, next) => {
+        console.log("------", req.file.filename)
+        const filledApplication = {
+            path: req.file.destination,
+            fileName: req.file.filename
+        }
+        console.log(filledApplication)
+        return res.status(201).send(response);
+        // publicationService.addFolderPathApplication(filledApplication, req.params.publicationId)
+        // .then(response => res.status(201).send(response))
+        // .catch(error => next(error));
+    });
 
 module.exports = managementRouter;
--- Backend/routes/student.routes.js
+++ Backend/routes/student.routes.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const userService = require("../service/management.service");
 const { studentAuth, adminAuth } = require('../middleware/auth.middleware');
+const { setPathAnswerSheet, upload } = require("../middleware/file-system.middleware");
 
 const studentRouter = express.Router();
 
 studentRouter.get("/student-profile/:studentId", studentAuth, (req, res, next) => {
     return userService.getStudentById(studentId)
         .then(response => {
-            if(response) {
+            if (response) {
                 return response;
             }
             throw new ApiError("Student not found", 404);
         });
-    
+});
+
+studentRouter.put("/answer-sheet-upload/:courseYearSem/:studentId",
+    studentAuth,
+    setPathAnswerSheet,
+    upload.single('file'),
+    (req, res, next) => {
+        console.log("------", req.file.filename)
+        const filledApplication = {
+            path: req.file.destination,
+            fileName: req.file.filename
+        }
+        console.log(filledApplication)
+        res = filledApplication
+        return res
+        // publicationService.addFolderPathApplication(filledApplication, req.params.publicationId)
+        // .then(response => res.status(201).send(response))
+        // .catch(error => next(error));
     });
 
 module.exports = studentRouter;
