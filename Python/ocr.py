ce8b978e40f80cc0e7e9ff545fed60245d8b0577
vaivassudhan <vaivassaravanan1@gmail.com>
2022-04-26 21:45:52 +0530

    python

---
+++ Python/ocr.py
@@ -0,0 +1,36 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+# </snippet_imports>
+
+'''
+Authenticate
+Authenticates your credentials and creates a client.
+''' 
+# <snippet_vars>
+subscription_key = "d5d0e372874a4c11ba463a30abe853f1"
+endpoint = "https://answer-sheet.cognitiveservices.azure.com/"
+# </snippet_vars>
+# </snippet_imports_and_vars>
+
+# <snippet_client>
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+from PIL import Image, ImageDraw
+read_image = open("../images/sent.jpeg", "rb")
+read_response = computervision_client.read_in_stream(read_image, raw=True)
+# Get the operation location (URL with ID as last appendage)
+read_operation_location = read_response.headers["Operation-Location"]
+operation_id = read_operation_location.split("/")[-1]
+while True:
+    read_result = computervision_client.get_read_result(operation_id)
+    if read_result.status.lower () not in ['notstarted', 'running']:
+        break
+    print ('Waiting for result...')
+    time.sleep(1)
